8ba0d5066df8dca06dc5cbfc58fcbf4c10380f94
Leslie Segovia <lesliesegovia1030@gmail.com>
2015-04-13 23:32:35 -0700

    kjb

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -50,12 +50,12 @@ game.HeroDeathManager = Object.extend({
 game.ExperienceManager = Object.extend({
     init: function(x, y, settings) {
         this.alwaysUpdate = true;
-        this.gameOver = false;
+        this.gameover = false;
     },
     update: function() {
-        if(game.data.win === true && !this.gameOver){
+        if(game.data.win === true && !this.gameover){
            this.gameOver(true);
-        }else if(game.data.win === false && !this.gameOver){
+        }else if(game.data.win === false && !this.gameover){
          this.gameOver(false);
         }
         return true;
@@ -67,8 +67,9 @@ game.ExperienceManager = Object.extend({
             }else{
             game.data.exp += 1;
         }
-         this.gameOver = true;
+         this.gameover = true;
              me.save.exp = game.data.exp;
+         
         }
     
 });
--- js/screens/title.js
+++ js/screens/title.js
@@ -5,33 +5,74 @@ game.TitleScreen = me.ScreenObject.extend({
     onResetEvent: function() {
         me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('title-screen')), -10); // TODO
 
-        me.input.bindKey(me.input.KEY.ENTER, "start");
 
         me.game.world.addChild(new (me.Renderable.extend({
             init: function() {
-                this._super(me.Renderable, 'init', [510, 30, me.game.viewport.width, me.game.viewport.height]);
+                this._super(me.Renderable, 'init', [270, 240, 300, 50]);
                 this.font = new me.Font("Ariel", 46, "white");
+                me.input.registerPointerEvet('pointerdown', this.newGame.bind(this), true);
                 
             },
             draw: function(renderer) {
-                this.font.draw(renderer.getContext(), "Revenge of the PewDuckPie", 450, 130);
-                this.font.draw(renderer.getContext(), "Press ENTER to play", 250, 530);
+                this.font.draw(renderer.getContext(), "START A NEW GAME", this.pos.x, this.pos.y);
+           
                 
+            },
+            
+            update: function(dt){
+                return true;
+            },
+            
+            newGame: function(){
+                me.input.releasePointerEvent('pointerdown', this);
+                me.save.remove('exp');
+                me.save.remove('exp1');
+                me.save.remove('exp2');
+                me.save.remove('exp3');
+                me.save.remove('exp4');
+                me.state.change(me.state.PLAY);
             }
 
         })));
         
-        this.handler = me.event.subscribe(me.event.KEYDOWN, function (action, keyCode, edge){
-           if(action === "start"){
-               me.state.change(me.state.PLAY);
-           } 
-        });
+         me.game.world.addChild(new (me.Renderable.extend({
+            init: function() {
+                this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+                this.font = new me.Font("Ariel", 46, "white");
+                me.input.registerPointerEvet('pointerdown', this.newGame.bind(this), true);
+                
+            },
+            draw: function(renderer) {
+                this.font.draw(renderer.getContext(), "CONTINUE", this.pos.x, this.pos.y);
+           
+                
+            },
+            
+            update: function(dt){
+                return true;
+            },
+            
+            newGame: function(){
+                game.data.exp = me.save.exp;
+                 game.data.exp1 = me.save.exp1;
+                  game.data.exp2 = me.save.exp2;
+                   game.data.exp3 = me.save.exp3;
+                    game.data.exp4 = me.save.exp4;
+                
+                me.input.releasePointerEvent('pointerdown', this);
+                me.state.change(me.state.PLAY);
+            }
+
+        })));
+        
+       
     },
     /**	
      *  action to perform when leaving this screen (state change)
      */
     onDestroyEvent: function() {
-       me.input.unbindKey(me.input.KEY.ENTER) ; // TODO
-       me.event.unsubscribe(this.handler);
+      
     }
 });
+
+
